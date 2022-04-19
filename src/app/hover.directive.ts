b35fea47f4a0b07e71e8abf581aa9098294f4a16
Dindihub <sadindi03@gmail.com>
2022-04-19 08:55:20 +0300

    Code-Add mouse event to hostlistener

--- src/app/hover.directive.ts
+++ src/app/hover.directive.ts
@@ -1,14 +1,23 @@
-import { Directive,ElementRef,OnInit } from '@angular/core';
+import { Directive,ElementRef,OnInit,HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHover]',
 })
 export class HoverDirective implements OnInit {
-
-  constructor(public nowHover: ElementRef) {}
+  constructor(public toChange: ElementRef) {}
 
   ngOnInit(): void {
-    this.nowHover.nativeElement.style.backgroundColor="orange"
+    // this.nowHover.nativeElement.style.backgroundColor="orange"
+  }
+  @HostListener('mouseenter') mouseEnter() {
+    this.changeColor('purple');
+  }
+
+  @HostListener('mouseleave') mouseLeave() {
+    this.changeColor('green');
+  }
+  changeColor(color: string) {
+    this.toChange.nativeElement.style.backgroundColor = 'color';
   }
 }
 
